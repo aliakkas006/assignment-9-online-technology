dcd0c64714345883cb6d8c18b578a26e2cf51d74
Ali Akkas <aliakkas006@gmail.com>
2021-10-04 20:06:16 +0600

    router added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className="text-center">
+            <h2 className="mb-5">About us:</h2>
+            <h4>Online Technology</h4>
+            <p>This is online based education system</p>
+            <p>Since 2009</p>
+            <p>Author: Ali Akkas</p>
+            <p>Email: ajfa@gmail.com</p>
         </div>
     );
 };
