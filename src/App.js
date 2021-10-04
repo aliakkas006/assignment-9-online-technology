7ade3ef6bc57b631e425ff2eba0bccf7bb262e43
Ali Akkas <aliakkas006@gmail.com>
2021-10-04 07:13:17 +0600

    fake date created

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Employee from "./components/Employee/Employee";
-import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import NotFound from "./components/NotFound/NotFound";
-import SingleEmployee from "./components/SingleEmployee/SingleEmployee";
-
 function App() {
 
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Switch>
-
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route exact path="/home">
-            <Home />
-          </Route>
-
-          <Route exact path="/employee">
-            <Employee />
-          </Route>
-
-          <Route exact path="/employee/:id">
-            <SingleEmployee />
-          </Route>
-
-          <Route exact path="*">
-            <NotFound />
-          </Route>
-
-        </Switch>
-      </Router>
     </div>
   );
 }
